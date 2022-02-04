114ef98468c51f80784c3c2df7810a3931a78ddb
Kirils Topčijs <kirilltopchy@gmail.com>
2022-02-04 14:58:16 +0200

    test-00 and test-01 are green.

---
+++ FlightPlanner_Web/FlightPlanner_Web/Controllers/AdminApiController.cs
@@ -0,0 +1,23 @@
+﻿using FlightPlanner_Web.Handlers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner_Web.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    [Authorize]
+    public class AdminApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Flights/{id}")]
+        public IActionResult GetFlights(int id)
+        {
+            if (!BasicAuthenticationHandler.isauthorized)
+            {
+                return Unauthorized();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ FlightPlanner_Web/FlightPlanner_Web/Controllers/TestingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner_Web.Controllers
+{
+    [Route("testing-api")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        [HttpPost]
+        [Route("clear")]
+        public IActionResult Clear(int id)
+        {
+            // ToDo: clear all flights, airports.
+            return Ok();
+        }
+    }
+}
